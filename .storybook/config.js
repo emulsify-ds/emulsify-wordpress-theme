79ba5ddf568c4e033135a1906620ae46dcd493a3
Evan Willhite <evan@fourkitchens.com>
2020-03-29 21:28:07 -0500

    cleaning up things, removing styleguide, moving authoritative components dir

--- .storybook/config.js
+++ .storybook/config.js
@@ -21,31 +21,28 @@ import '../components/style.scss';
 addDecorator(withA11y);
 
 const Twig = require('twig');
-const twigDrupal = require('twig-drupal-filters');
 const twigBEM = require('bem-twig-extension');
 const twigAddAttributes = require('add-attributes-twig-extension');
 
 Twig.cache();
 
-twigDrupal(Twig);
 twigBEM(Twig);
 twigAddAttributes(Twig);
 
-// If in a Drupal project, it's recommended to import a symlinked version of drupal.js.
-import './_drupal.js';
+import './_attach.js';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('western-up-twig', true, /\.stories\.js$/), module);
+configure(require.context('../components', true, /\.stories\.js$/), module);
 
-// Gatsby's Link overrides:
-// Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
-global.___loader = {
-  enqueue: () => {},
-  hovering: () => {},
-};
-// Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
-global.__PATH_PREFIX__ = '';
-// This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
-window.___navigate = (pathname) => {
-  action('NavigateTo:')(pathname);
-};
+// // Gatsby's Link overrides:
+// // Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
+// global.___loader = {
+//   enqueue: () => {},
+//   hovering: () => {},
+// };
+// // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
+// global.__PATH_PREFIX__ = '';
+// // This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
+// window.___navigate = (pathname) => {
+//   action('NavigateTo:')(pathname);
+// };
---
+++ components/01-atoms/buttons/buttons.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Button from './react/Button.component';
+
+import button from './twig/button.twig';
+
+import buttonData from './twig/button.yml';
+import buttonAltData from './twig/button-alt.yml';
+import buttonAlt2Data from './twig/button-alt2.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default {
+  component: Button,
+  title: 'Atoms/Button',
+};
+
+export const react = () => <Button>React Button</Button>;
+
+export const twig = () => (
+  <div dangerouslySetInnerHTML={{ __html: button(buttonData) }} />
+);
+export const twigAlt = () => (
+  <div dangerouslySetInnerHTML={{ __html: button(buttonAltData) }} />
+);
+export const twigAlt2 = () => (
+  <div dangerouslySetInnerHTML={{ __html: button(buttonAlt2Data) }} />
+);
