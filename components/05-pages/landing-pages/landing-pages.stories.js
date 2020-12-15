57ea6069c3a894aa69723bcd4e1ae429c02d4e75
Evan Willhite <evan@fourkitchens.com>
2020-12-15 17:08:07 -0600

    full page fixes and final package upgrades

--- components/05-pages/landing-pages/landing-pages.stories.js
+++ components/05-pages/landing-pages/landing-pages.stories.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { hrefTo } from '@storybook/addon-links';
-import { useEffect } from '@storybook/client-api';
 
 import '../../02-molecules/menus/main-menu/main-menu';
 
@@ -15,24 +12,24 @@ import footerMenuData from '../../02-molecules/menus/inline/inline-menu.yml';
 /**
  * Storybook Definition.
  */
-hrefTo('Pages/Content Types', 'Article').then(url => {
-  // TODO: Can't figure out how to link pages with hrefTo and storiesOf.
-  storiesOf('Pages/Landing Pages', module).add('Home', () => {
-    useEffect(() => Attach.attachBehaviors(), []);
-    return (
-      <div
-        dangerouslySetInnerHTML={{
-          __html: home({
-            page_layout_modifier: 'contained',
-            ...mainMenuData,
-            ...breadcrumbData,
-            ...socialMenuData,
-            ...footerMenuData,
-            card_link_url: url,
-            card__link__text: 'Click here',
-          }),
-        }}
-      />
-    );
-  });
-});
+export default {
+  title: 'Pages/Landing Pages',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const homePage = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html: home({
+        page_layout_modifier: 'contained',
+        ...mainMenuData,
+        ...breadcrumbData,
+        ...socialMenuData,
+        ...footerMenuData,
+        card__link__text: 'Click here',
+      }),
+    }}
+  />
+);
