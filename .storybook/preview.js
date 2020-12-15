90644dd7d05ec64778a88d31d8da292b87ca23cf
Evan Willhite <evan@fourkitchens.com>
2020-12-15 12:52:00 -0600

    storybook upgrade, changes for v6

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+import { addDecorator } from '@storybook/react';
+import { useEffect } from '@storybook/client-api';
+import Twig from 'twig';
+import { setupTwig } from './setupTwig';
+
+// GLOBAL CSS
+import '../components/style.scss';
+
+// If in a Drupal project, it's recommended to import a symlinked version of drupal.js.
+import './_drupal.js';
+
+// addDecorator deprecated, but not sure how to use this otherwise.
+addDecorator((storyFn) => {
+  useEffect(() => Drupal.attachBehaviors(), []);
+  return storyFn();
+});
+
+setupTwig(Twig);
