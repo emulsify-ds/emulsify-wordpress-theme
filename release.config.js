644f78f65130285b7f022676675aa396e3cfac6a
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2026-06-27 04:01:13 -0500

    ci: replace release workflow for wordpress

--- release.config.js
+++ release.config.js
@@ -1,10 +1,17 @@
+const parserOpts = {
+  headerPattern: /^(\w*)(?:\(([\w$.\-*/ ]*)\))?!?: (.*)$/,
+  headerCorrespondence: ['type', 'scope', 'subject'],
+  breakingHeaderPattern: /^(\w*)(?:\(([\w$.\-*/ ]*)\))?!: (.*)$/,
+  noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING-CHANGE'],
+};
+
 module.exports = {
   tagFormat: '${version}',
   branches: ['main'],
   repositoryUrl: 'git@github.com:emulsify-ds/emulsify-wordpress-theme.git',
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    ['@semantic-release/commit-analyzer', { parserOpts }],
+    ['@semantic-release/release-notes-generator', { parserOpts }],
     ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/github',
   ],
